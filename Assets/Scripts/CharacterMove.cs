3e4fc80dd7af7d5e701e7b4e2b87e0f61736d493
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2018-11-04 18:22:52 +0100

    Movement and testing of PlayerPrefs(Saving)
    What the title said

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    CharacterController controller;
+    float speed = 5.0f;
+    float gravity = 3.5f;
+    float jumpForce = 2.5f;
+    float vertivalVelocity;
+
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+
+	void FixedUpdate () {
+
+        float moveForward = Input.GetAxis("Forward") * speed * Time.deltaTime;
+        float moveBack = Input.GetAxis("Backward") * speed * Time.deltaTime;
+        float moveLeft = Input.GetAxis("Left") * speed * Time.deltaTime;
+        float moveRight = Input.GetAxis("Right") * speed * Time.deltaTime;
+
+        if (controller.isGrounded)
+        {
+            vertivalVelocity = -gravity * Time.deltaTime;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertivalVelocity = jumpForce;
+            }
+        } else
+        {
+            vertivalVelocity -= gravity * Time.deltaTime;
+        }
+
+        controller.Move(new Vector3(moveRight + moveLeft, vertivalVelocity, moveForward + moveBack));
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public bool Save;
+    [Space(2)]
+    [Header("First gameobject should be the player")]
+    public GameObject[] spawnPrefabs;
+    Vector3 v3_spawnPos;
+    Quaternion spawnRot;
+    GameObject playerObject;
+
+	void Start () {
+        v3_spawnPos = new Vector3(PlayerPrefs.GetFloat("posX"), PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
+        spawnRot = new Quaternion(0, 0, 0, PlayerPrefs.GetFloat("rotW"));
+
+        Instantiate(spawnPrefabs[0], v3_spawnPos, spawnRot);
+
+        playerObject = GameObject.Find("Player(Clone)");
+    }
+
+    void Update()
+    {
+        if (Save)
+        {
+            PlayerPrefs.SetFloat("rotW", playerObject.transform.rotation.w);
+            PlayerPrefs.SetFloat("posX", playerObject.transform.position.x);
+            PlayerPrefs.SetFloat("posY", playerObject.transform.position.y);
+            PlayerPrefs.SetFloat("posZ", playerObject.transform.position.z);
+            PlayerPrefs.Save();
+            Debug.Log("Saved");
+            Save = false;
+        }
+    }
+}
